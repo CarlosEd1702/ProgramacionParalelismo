fb3427034c8c8f2befd23b8a5ffed5cd07e4ec43
CarlosEd1702 <112498922+CarlosEd1702@users.noreply.github.com>
2024-09-05 19:37:27 -0600

    Tarea 1

---
+++ Calculadora Tarea 1/Calculadora Tarea 1/kernel.cu
@@ -0,0 +1,171 @@
+﻿#include <iostream>
+#include <cuda_runtime.h>
+
+using namespace std;
+
+// Función para la suma en la CPU
+void add_cpu(int* a, int* b, int* c) {
+    *c = *a + *b;
+}
+
+// Función para la suma en la GPU
+__global__ void add_gpu(int* a, int* b, int* c) {
+    *c = *a + *b;
+}
+
+// Función para la resta en la CPU
+void subtract_cpu(int* a, int* b, int* c) {
+    *c = *a - *b;
+}
+
+// Función para la resta en la GPU
+__global__ void subtract_gpu(int* a, int* b, int* c) {
+    *c = *a - *b;
+}
+
+// Función para la multiplicar en la CPU
+void multiply_cpu(int* a, int* b, int* c) {
+    *c = *a * *b;
+}
+
+// Función para la multiplicar en la GPU
+__global__ void multiply_gpu(int* a, int* b, int* c) {
+    *c = *a * *b;
+}
+
+// Función para la dividir en la CPU
+void divide_cpu(int* a, int* b, int* c) {
+    *c = *a / *b;
+}
+
+// Función para la dividir en la GPU
+__global__ void divide_gpu(int* a, int* b, int* c) {
+    *c = *a / *b;
+}
+
+int main() {
+    // Variables en el Host (CPU)
+    int operacion, a, b, c_cpu;
+    char continuar;
+
+    // Variables en el Device (GPU)
+    int* d_a, * d_b, * d_c;
+    int c_gpu;
+
+    // Asignar memoria en la GPU para a, b y c
+    cudaMalloc((void**)&d_a, sizeof(int));
+    cudaMalloc((void**)&d_b, sizeof(int));
+    cudaMalloc((void**)&d_c, sizeof(int));
+
+    do {
+        // Decidir cuál operación hacer
+        cout << "Programacion Paralelismo - Tarea 1" << endl;
+        cout << "\nQue operacion quieres hacer?" << endl;
+        cout << "1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n" << endl;
+        cin >> operacion;
+
+        system("CLS");
+
+        // Pedir los valores de entrada al usuario
+        cout << "Ingresa el primer numero: ";
+        cin >> a;
+        cout << "\nIngresa el segundo numero: ";
+        cin >> b;
+
+        // Copiar los valores a y b del Host a la GPU
+        cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
+
+        switch (operacion) {
+        case 1:
+            // Suma en la CPU
+            add_cpu(&a, &b, &c_cpu);
+            cout << "Resultado de la suma en CPU: " << c_cpu << endl;
+            cout << "Direccion de memoria del resultado en CPU: " << &c_cpu << endl;
+
+            // Ejecutar la función de suma en la GPU
+            add_gpu << <1, 1 >> > (d_a, d_b, d_c);
+
+            // Copiar el resultado de la GPU de vuelta al Host
+            cudaMemcpy(&c_gpu, d_c, sizeof(int), cudaMemcpyDeviceToHost);
+
+            // Mostrar el resultado de la suma en GPU
+            cout << "Resultado de la suma en GPU: " << c_gpu << endl;
+            cout << "Direccion de memoria del resultado en GPU (en Host): " << &c_gpu << endl;
+            cout << "Direccion de memoria del resultado en GPU (en Device): " << d_c << endl;
+            break;
+
+        case 2:
+            // Resta en la CPU
+            subtract_cpu(&a, &b, &c_cpu);
+            cout << "Resultado de la resta en CPU: " << c_cpu << endl;
+            cout << "Direccion de memoria del resultado en CPU: " << &c_cpu << endl;
+
+            // Ejecutar la función de resta en la GPU
+            subtract_gpu << <1, 1 >> > (d_a, d_b, d_c);
+
+            // Copiar el resultado de la GPU de vuelta al Host
+            cudaMemcpy(&c_gpu, d_c, sizeof(int), cudaMemcpyDeviceToHost);
+
+            // Mostrar el resultado de la resta en GPU
+            cout << "Resultado de la resta en GPU: " << c_gpu << endl;
+            cout << "Dirección de memoria del resultado en GPU (en Host): " << &c_gpu << endl;
+            cout << "Dirección de memoria del resultado en GPU (en Device): " << d_c << endl;
+            break;
+
+        case 3:
+            // Multiplicacion en la CPU
+            multiply_cpu(&a, &b, &c_cpu);
+            cout << "Resultado de la multiplicacion en CPU: " << c_cpu << endl;
+            cout << "Direccion de memoria del resultado en CPU: " << &c_cpu << endl;
+
+            // Ejecutar la función de multiplicacion en la GPU
+            multiply_gpu << <1, 1 >> > (d_a, d_b, d_c);
+
+            // Copiar el resultado de la GPU de vuelta al Host
+            cudaMemcpy(&c_gpu, d_c, sizeof(int), cudaMemcpyDeviceToHost);
+
+            // Mostrar el resultado de la multiplicacion en GPU
+            cout << "Resultado de la multiplicacion en GPU: " << c_gpu << endl;
+            cout << "Dirección de memoria del resultado en GPU (en Host): " << &c_gpu << endl;
+            cout << "Dirección de memoria del resultado en GPU (en Device): " << d_c << endl;
+            break;
+
+        case 4:
+            // Division en la CPU
+            divide_cpu(&a, &b, &c_cpu);
+            cout << "Resultado de la division en CPU: " << c_cpu << endl;
+            cout << "Dirección de memoria del resultado en CPU: " << &c_cpu << endl;
+
+            // Ejecutar la función de división en la GPU
+            divide_gpu << <1, 1 >> > (d_a, d_b, d_c);
+
+            // Copiar el resultado de la GPU de vuelta al Host
+            cudaMemcpy(&c_gpu, d_c, sizeof(int), cudaMemcpyDeviceToHost);
+
+            // Mostrar el resultado de la division en GPU
+            cout << "Resultado de la division en GPU: " << c_gpu << endl;
+            cout << "Direccion de memoria del resultado en GPU (en Host): " << &c_gpu << endl;
+            cout << "Direccion de memoria del resultado en GPU (en Device): " << d_c << endl;
+            break;
+
+        default:
+            cout << "Operacion no valida!" << endl;
+            break;
+        }
+
+        // Preguntar al usuario si quiere continuar
+        cout << "\nDeseas realizar otra operacion? (S/N): ";
+        cin >> continuar;
+
+        system("CLS");  
+
+    } while (continuar == 'S' || continuar == 's');
+
+    // Liberar memoria en la GPU
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+    return 0;
+}
---
+++ Tarea1/Tarea1/kernel.cu
@@ -0,0 +1,80 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+__global__ void cuda_hello() {
+	printf("Hello World from GPU\n");
+}
+
+void add_cpu(int* a, int* b, int* c) {
+
+	*c = *a + *b;
+}
+
+__global__ void add_gpu(int *a, int *b, int *c) {
+	
+	*c = *a + *b;
+	
+	/*int example;
+	int* ptr_example = &example;
+	int** ptr_ptr_example = &ptr_example;
+
+	int example2 = *ptr_example;*/
+}
+
+int main() {
+	int a = 2;
+	int* b;
+	int c;
+	b = &c;
+	*b = 5;
+	c = 5;
+	int result;
+
+	float a_float = (float)a;
+	char letra_a = 'a';
+
+	int* a_gpu, *b_gpu, *result_gpu;
+
+	int size = sizeof(int);
+	int size_a = sizeof(a);
+
+
+
+	//cout << "letra a: " << letra_a << " letra_a a int " << (int)letra_a << endl;
+
+	add_cpu(&a, b, &result);
+	add_gpu << <1, 1 >> > (&a, b, &result);
+	
+	cout << "El resultado de la suma en CPU es: " << result << "\n";
+
+	// cout << "Int: " << size << " size_a: " << size_a << endl;
+	//cuda_hello <<<1, 10>>> ();
+
+	/*cudaMalloc((void**)&a_gpu, size);
+	cudaMalloc((void**)&b_gpu, size);
+	cudaMalloc((void**)&result_gpu, size);
+
+	cudaMemcpy(a_gpu, &a, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(b_gpu, &c, size, cudaMemcpyHostToDevice);
+	
+	cudaMemcpy(&result, result_gpu, size, cudaMemcpyDeviceToHost);
+
+	cout << "Result " << result << endl;
+	printf("Result %d", result);
+
+	cudaFree(a_gpu);
+	cudaFree(b_gpu);
+	cudaFree(result_gpu);*/
+
+	return 0;
+}
+
+//int main() {
+//	cout << "Hello World!" << endl;
+//	return 0;
+//}
